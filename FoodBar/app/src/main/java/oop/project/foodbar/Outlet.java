05dcfce1a89058b84dbc303a03f82fdb980256e3
Aashi Aggarwal <aashiagg22@gmail.com>
2020-04-02 11:59:46 +0530

    Update Menus and Restaurants

---
+++ FoodBar/app/src/main/java/oop/project/foodbar/Outlet.java
@@ -0,0 +1,14 @@
+package oop.project.foodbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Outlet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outlet);
+    }
+}
